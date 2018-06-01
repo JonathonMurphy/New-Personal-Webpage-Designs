215783e9a6634979b432d221c07b0fc8a3613385
JonathonMurphy <JSMurphy1991@gmail.com>
2018-06-01 13:59:24 -0400

    Added animation

--- js/04-29-2018.js
+++ js/04-29-2018.js
@@ -1,47 +1,50 @@
 $(function() {
-  //Note to self, the margin-top resetting could probably be done better
-  //in css @media querys or with a more exact formula
-  var $documentHeight = $(document).height();
-  var $documentWidth = $(document).width();
-  var $viewportHeight = $(window).height();
-  var $viewportWidth = $(window).width();
+  var $slideCardOneHTML = $('#slideCardOneHTML');
+  var $slideCardTwoHTML = $('#slideCardTwoHTML');
   var $containerFluid = $('.container-fluid');
   var $contentContainer = $('.content-container');
-  //Set the margin-top of .content-container if the viewport width is
-  //smaller than the viewport height
-  if ($viewportWidth < $viewportHeight && $viewportHeight >= 1000) {
-    var portraitMarginTop = ($viewportHeight - 77) / 4;
-    $contentContainer.css({'margin-top': portraitMarginTop});
-  }
+  var $contentBottomOffset = $('#contentBottom').offset();
+  var contentContainerMarginTop = (($(document).height() - $contentBottomOffset.top) / 2.2337662337662337662337662337662) - 2;
+  //Set the margin-top of .content-container as determined by the top
+  //offset of contentBottom, and $(document).height()
+  $contentContainer.css({'margin-top': contentContainerMarginTop});
+  //Show page after margin-top set
+  $('body').css({'opacity': 100});
+  //Animate slideCards 
+  $slideCardOneHTML.css({'display': 'block'});
+  $slideCardTwoHTML.css({'display': 'block'});
+  $slideCardOneHTML.animate({
+    opacity: 100.0,
+    marginRight: '-=200'
+  }, 1200);
+  $slideCardTwoHTML.animate({
+    opacity: 100.0,
+    marginRight: '-=200'
+  }, 1200);
   //Set height of .container-fluid so that the background will reach the
   //bottom of the page regardless of document or window(viewport) height.
-  if ($documentHeight >= $viewportHeight) {
-    $containerFluid.css({'height': $documentHeight});
+  if ($(document).height() >= $(window).height()) {
+    $containerFluid.css({'height': $(document).height()});
   } else {
-    $containerFluid.css({'height': $viewportHeight});
+    $containerFluid.css({'height': $(window).height()});
   }
 });
 //on resize event listener to adjust layout when resized
 $(window).resize(function() {
-  var $documentHeight = $(document).height();
-  var $documentWidth = $(document).width();
-  var $viewportHeight = $(window).height();
-  var $viewportWidth = $(window).width();
   var $containerFluid = $('.container-fluid');
   var $contentContainer = $('.content-container');
-  //Set the margin-top of .content-container if the viewport width is
-  //smaller than the viewport height
-  if ($viewportWidth < $viewportHeight && $viewportHeight >= 1000) {
-    var portraitMarginTop = ($viewportHeight - 77) / 4;
-    $contentContainer.css({'margin-top': portraitMarginTop});
-  } else {
-    $contentContainer.css({'margin-top': 77});
-  }
+  var $contentBottomOffset = $('#contentBottom').offset();
+  var contentContainerMarginTop = (($(document).height() - $contentBottomOffset.top) / 2.2337662337662337662337662337662) - 2;
+  //Set the margin-top of .content-container as determined by the top
+  //offset of contentBottom, and $(document).height()
+  $contentContainer.css({'margin-top': contentContainerMarginTop});
+  //Rest $containerFluid height to 0
+  $containerFluid.css({'height': 0});
   //Set height of .container-fluid so that the background will reach the
   //bottom of the page regardless of document or window(viewport) height.
-  if ($documentHeight >= $viewportHeight) {
-    $containerFluid.css({'height': $documentHeight});
+  if ($(document).height() >= $(window).height()) {
+    $containerFluid.css({'height': $(document).height()});
   } else {
-    $containerFluid.css({'height': $viewportHeight});
+    $containerFluid.css({'height': $(window).height()});
   }
 });
