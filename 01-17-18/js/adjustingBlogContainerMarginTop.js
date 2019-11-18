d7a931daf0b5b89462dcd72c792656f598829deb
Jonathon Murphy <jsmurphy1991@gmail.com>
2019-11-18 11:20:20 -0500

    reorganizing file structure

--- 01-17-18/js/adjustingBlogContainerMarginTop.js
+++
@@ -1,7 +0,0 @@
-function height() {
-	var blogContainer = document.getElementById("blog-container");
-	var blogContainerHeight = document.getElementById("blog-container").clientHeight; 
-	var marginHeight = blogContainerHeight - 900; 
-	blogContainer.style.marginTop = marginHeight + "px";
-}
-height();
